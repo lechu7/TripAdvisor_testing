452246eea71567098d4387b460dbb48728083e71
Łukaś <lukaszlaszuk@gmail.com>
2019-11-11 14:06:22 +0100

    Add files via upload

--- TripAdvisor_testing/GlobalVariables.cs
+++ TripAdvisor_testing/GlobalVariables.cs
@@ -19,5 +19,11 @@ namespace TripAdvisor_testing
         public static string emailNegative = "a@a.com";
         public static string passwordNegative = "ZleHaslo";
 
+        //rejestracja prawidłowe dane - Łukaś
+        public static string registerNameCorrect = "Alfred";
+        public static string registerSurnameCorrect = "Kamyczek";
+        public static string registerEmailCorrect = "alfredkamyczek@wzim.com";
+        public static string registerPasswordCorrect = "Abcdefgh1";
+        public static string registerRepeatPasswordCorrect = "Abcdefgh1";
     }
 }
