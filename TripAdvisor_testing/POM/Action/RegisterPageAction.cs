8b8eca13952f8712ea98c2755c69281434faf9e2
Łukaś <lukaszlaszuk@gmail.com>
2019-11-11 14:08:47 +0100

    Add files via upload

--- TripAdvisor_testing/POM/Action/RegisterPageAction.cs
+++ TripAdvisor_testing/POM/Action/RegisterPageAction.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,54 @@ namespace TripAdvisor_testing
         public RegisterPageAction(IWebDriver driver) : base(driver)
         {
 
+        }
+        public void registerTitleTXValidate()
+        {
+            Assert.IsTrue(registerTitleTX.Displayed);
+        }
+        public void registerNameTXValidate()
+        {
+            Assert.IsTrue(registerNameTX.Displayed);
+        }
+        public void registerNameETSet(String value)
+        {
+            registerNameET.SendKeys(value);
+        }
+        public void registerSurnameTXValidate()
+        {
+            Assert.IsTrue(registerSurnameTX.Displayed);
+        }
+        public void registerSurnameETSet(String value)
+        {
+            registerSurnameET.SendKeys(value);
+        }
+        public void registerEmailTXValidate()
+        {
+            Assert.IsTrue(registerEmailTX.Displayed);
+        }
+        public void registerEmailETSet(String value)
+        {
+            registerEmailET.SendKeys(value);
+        }
+        public void registerPasswordTXValidate()
+        {
+            Assert.IsTrue(registerPasswordTX.Displayed);
+        }
+        public void registerPasswordETSet(String value)
+        {
+            registerPasswordET.SendKeys(value);
+        }
+        public void registerRepeatPasswordTXValidate()
+        {
+            Assert.IsTrue(registerRepeatPasswordTX.Displayed);
+        }
+        public void registerRepeatPasswordETSet(String value)
+        {
+            registerRepeatPasswordET.SendKeys(value);
+        }
+        public void registerBTClick()
+        {
+            registerBT.Click();
         }
     }
 }
--- TripAdvisor_testing/POM/View/RegisterPageView.cs
+++ TripAdvisor_testing/POM/View/RegisterPageView.cs
@@ -17,6 +17,40 @@ namespace TripAdvisor_testing
             PageFactory.InitElements(driver, this);
         }
 
+        [FindsBy(How = How.XPath, Using = "//h1[text()='zarejestruj ']")]
+        protected IWebElement registerTitleTX;
 
+        [FindsBy(How = How.XPath, Using = "//label[text()='Imię']")]
+        protected IWebElement registerNameTX;
+
+        [FindsBy(How = How.Id, Using = "name")]
+        protected IWebElement registerNameET;
+
+        [FindsBy(How = How.XPath, Using = "//label[text()='Nazwisko']")]
+        protected IWebElement registerSurnameTX;
+
+        [FindsBy(How = How.Id, Using = "surname")]
+        protected IWebElement registerSurnameET;
+
+        [FindsBy(How = How.XPath, Using = "//label[text()='E-mail']")]
+        protected IWebElement registerEmailTX;
+
+        [FindsBy(How = How.Id, Using = "email")]
+        protected IWebElement registerEmailET;
+
+        [FindsBy(How = How.XPath, Using = "//label[text()='Hasło']")]
+        protected IWebElement registerPasswordTX;
+
+        [FindsBy(How = How.Id, Using = "password")]
+        protected IWebElement registerPasswordET;
+
+        [FindsBy(How = How.XPath, Using = "//label[text()='Powtórz hasło']")]
+        protected IWebElement registerRepeatPasswordTX;
+
+        [FindsBy(How = How.Id, Using = "repeatPassword")]
+        protected IWebElement registerRepeatPasswordET;
+
+        [FindsBy(How = How.XPath, Using = "//button[@type='submit']")]
+        protected IWebElement registerBT;
     }
 }
